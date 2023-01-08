7989dfadfdc2fc82d97d98a2ffa768ff72a8d8b2
Kortina <kortina@gmail.com>
2023-01-08 17:12:25 -0500

    add yargs and eslintrc

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,40 @@
 #!/usr/bin/env node
-const d = `${new Date()}`;
-console.log(d);
-
 import { Fountain } from 'fountain-js';
-let fountain = new Fountain();
+import yargs = require('yargs');
+// import { yargs } from 'yargs';
 
-console.log(d);
+const main = () => {
+  const argv = yargs
+    .command('lyr', 'Tells whether an year is leap year or not', {
+      year: {
+        description: 'the year to check for',
+        alias: 'y',
+        type: 'number',
+      },
+    })
+    .option('time', {
+      alias: 't',
+      description: 'Tell the present Time',
+      type: 'boolean',
+    })
+    .help()
+    .alias('help', 'h').argv;
+
+  const d = `${new Date()}`;
+  console.log(d);
+
+  let fountain = new Fountain();
+
+  console.log(d);
+};
 
 export const foo = () => {
   return 1;
 };
+
+// Check if we are running as a cli
+// Can't use === module because we have a lightweight wrapper at
+// bin/fountain-stats
+if (typeof require !== 'undefined' && require.main?.filename?.match(/fountain-stats/)) {
+  main();
+}
