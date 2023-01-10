385d5b4022801e62155f0b99f9437b51d7ce575d
Kortina <kortina@gmail.com>
2023-01-09 22:43:39 -0500

    add calcLines and some tests

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,102 @@
 #!/usr/bin/env node
-import { Fountain } from 'fountain-js';
+import { Fountain, Token } from 'fountain-js';
 import yargs = require('yargs');
 import { readFileSync } from 'fs';
+import wrap = require('word-wrap');
+import { listenerCount } from 'process';
 
-// import { yargs } from 'yargs';
+enum TokenType {
+  title = 'title',
+  credit = 'credit',
+  author = 'author',
+  authors = 'authors',
+  source = 'source',
+  notes = 'notes',
+  draft_date = 'draft_date',
+  date = 'date',
+  contact = 'contact',
+  copyright = 'copyright',
+  scene_heading = 'scene_heading',
+  transition = 'transition',
+  dual_dialogue_begin = 'dual_dialogue_begin',
+  dialogue_begin = 'dialogue_begin',
+  character = 'character',
+  parenthetical = 'parenthetical',
+  dialogue = 'dialogue',
+  dialogue_end = 'dialogue_end',
+  dual_dialogue_end = 'dual_dialogue_end',
+  section = 'section',
+  synopsis = 'synopsis',
+  note = 'note',
+  boneyard_begin = 'boneyard_begin',
+  boneyard_end = 'boneyard_end',
+  action = 'action',
+  centered = 'centered',
+  lyrics = 'lyrics',
+  page_break = 'page_break',
+  line_break = 'line_break',
+}
+
+const tokenType = (key: string): TokenType => {
+  let typedKey = key as keyof typeof TokenType;
+  return TokenType[typedKey];
+};
+
+const tokenIs = (token: Token, tokenType: TokenType): boolean => {
+  let tokenTypeString = TokenType[tokenType];
+  return token.type == tokenTypeString;
+};
+
+const numWrappedLines = (text: string, charsPerLine: number): Number => {
+  let wrapped = wrap(text, { width: charsPerLine });
+  let lines = wrapped.split('\n');
+  return lines.length;
+};
+
+export const calcLines = (token: Token): Number => {
+  let text = token.text || '';
+  switch (tokenType(token.type)) {
+    case TokenType.scene_heading:
+    case TokenType.action:
+    case TokenType.transition:
+    case TokenType.centered:
+      return numWrappedLines(text, 63);
+      break;
+    case TokenType.character:
+    case TokenType.dialogue:
+      return numWrappedLines(text, 34);
+      break;
+    case TokenType.parenthetical:
+      return numWrappedLines(text, 26);
+      break;
+    case TokenType.lyrics:
+      return numWrappedLines(text, 48);
+      break;
+    case TokenType.line_break:
+      return 1;
+    case TokenType.title:
+    case TokenType.credit:
+    case TokenType.author:
+    case TokenType.authors:
+    case TokenType.source:
+    case TokenType.notes:
+    case TokenType.draft_date:
+    case TokenType.date:
+    case TokenType.contact:
+    case TokenType.copyright:
+    case TokenType.dual_dialogue_begin: // TODO:
+    case TokenType.dialogue_end: // TODO:
+    case TokenType.dual_dialogue_end: // TODO:
+    case TokenType.section:
+    case TokenType.synopsis:
+    case TokenType.note:
+    case TokenType.boneyard_begin:
+    case TokenType.boneyard_end:
+    case TokenType.page_break:
+    default:
+      return 0;
+  }
+};
 
 interface Arguments {
   fountainFile: string;
@@ -26,7 +119,13 @@ const main = () => {
       //   console.log(text);
       let fountain = new Fountain();
       let f = fountain.parse(text, true);
-      console.log(f.tokens);
+      f.tokens?.forEach((t) => {
+        console.log(tokenType(t.type));
+        if (tokenIs(t, TokenType.scene_heading)) {
+          console.log('-------------------');
+        }
+      });
+      console.log(TokenType['scene_heading'] === TokenType.scene_heading);
     }
   ).argv;
 
--- src/test/jest/index.test.ts
+++ src/test/jest/index.test.ts
@@ -1,6 +1,23 @@
 import 'jest';
-import { foo } from '../../index';
+import { foo, calcLines } from '../../index';
 
 test('foo', () => {
   expect(foo()).toBe(1);
+});
+
+test('calcLines', () => {
+  let ten = '1234 6789 ';
+  let twenty = ten + ten;
+  let forty = twenty + twenty;
+  let eighty = forty + forty;
+  let oneSixty = eighty + eighty;
+  expect(calcLines({ type: 'scene_heading', text: twenty })).toBe(1);
+  expect(calcLines({ type: 'scene_heading', text: forty })).toBe(1);
+  expect(calcLines({ type: 'scene_heading', text: eighty })).toBe(2);
+  expect(calcLines({ type: 'character', text: twenty })).toBe(1);
+  expect(calcLines({ type: 'character', text: forty })).toBe(2);
+  expect(calcLines({ type: 'action', text: twenty })).toBe(1);
+  expect(calcLines({ type: 'action', text: forty })).toBe(1);
+  expect(calcLines({ type: 'action', text: eighty })).toBe(2);
+  expect(calcLines({ type: 'action', text: oneSixty })).toBe(3);
 });
