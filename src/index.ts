4e5a0bdaba114b77f22faa2a626aeef3f635ce31
Kortina <kortina@gmail.com>
2023-01-11 19:06:19 -0500

    fix line counts. fix scene head parser. add character string cleaner

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,13 @@ import yargs = require('yargs');
 import { readFileSync } from 'fs';
 import wrap = require('word-wrap');
 import { listenerCount } from 'process';
+import { debug } from 'console';
+const DEBUG = true;
+const dbg = (msg: string) => {
+  if (DEBUG) {
+    debug(msg);
+  }
+};
 
 enum TokenType {
   title = 'title',
@@ -37,6 +44,47 @@ enum TokenType {
   line_break = 'line_break',
 }
 
+interface SceneHeader {
+  rawHead: string;
+  ie: string;
+  loc: string;
+  tod: string;
+}
+
+export const cleanCharacter = (raw: string): string => {
+  let t = raw.trim();
+  // remove (O.S.) (V.O.) (CONT'D) etc
+  t = raw.replaceAll(/\([^\)]+\)/g, '');
+  t = t.trim();
+  // remove dual
+  t = t.replaceAll(/\s*[\^]+\s*/g, '');
+  t = t.replaceAll(/^\@+/g, '');
+  return t.trim();
+};
+export const parseSceneHeading = (t: string) => {
+  let sh = {
+    rawHead: t,
+    ie: '-',
+    loc: '-',
+    tod: '-',
+  };
+
+  // parse leading INT./EXT.
+  let ieRegex = /^([\S]+\. )/;
+  let ieMatch = t.match(ieRegex);
+  if (ieMatch) {
+    sh.ie = ieMatch[1].trim();
+    // strip it
+    t = t.replace(ieRegex, '');
+  }
+  let parts = t.split(' - ');
+  sh.loc = parts[0];
+  if (parts.length > 1) {
+    sh.tod = parts[1];
+  }
+  return sh;
+};
+
 const tokenType = (key: string): TokenType => {
   let typedKey = key as keyof typeof TokenType;
   return TokenType[typedKey];
@@ -47,13 +95,13 @@ const tokenIs = (token: Token, tokenType: TokenType): boolean => {
   return token.type == tokenTypeString;
 };
 
-const numWrappedLines = (text: string, charsPerLine: number): Number => {
+const numWrappedLines = (text: string, charsPerLine: number): number => {
   let wrapped = wrap(text, { width: charsPerLine });
   let lines = wrapped.split('\n');
   return lines.length;
 };
 
-export const calcLines = (token: Token): Number => {
+export const calcLines = (token: Token): number => {
   let text = token.text || '';
   switch (tokenType(token.type)) {
     case TokenType.scene_heading:
@@ -84,9 +132,9 @@ export const calcLines = (token: Token): Number => {
     case TokenType.date:
     case TokenType.contact:
     case TokenType.copyright:
-    case TokenType.dual_dialogue_begin: // TODO:
-    case TokenType.dialogue_end: // TODO:
-    case TokenType.dual_dialogue_end: // TODO:
+    case TokenType.dual_dialogue_begin:
+    case TokenType.dialogue_end:
+    case TokenType.dual_dialogue_end:
     case TokenType.section:
     case TokenType.synopsis:
     case TokenType.note:
@@ -98,57 +146,180 @@ export const calcLines = (token: Token): Number => {
   }
 };
 
-class Scene {
-  rawText: string;
-  ie: string;
-  loc: string | undefined;
-  tod: string | undefined;
-  sceneNumber: string | undefined;
-  numLines: Number;
+class PriorContext {
+  linesPerPage = 54;
+  scene?: Scene;
+  synopsis?: string;
+  numLines: number;
+  dualDialogContext: boolean;
+  dualDialogNumLines: number;
+  prevToken?: Token;
+  constructor(scene?: Scene, synopsis?: string, numLines?: number, dualDialogNumLines?: number) {
+    this.scene = scene;
+    this.synopsis = synopsis;
+    this.numLines = numLines || 0;
+    this.dualDialogNumLines = dualDialogNumLines || 0;
+    this.dualDialogContext = false;
+  }
 
-  constructor(
-    rawText: string,
-    ie: string,
-    loc: string | undefined,
-    tod: string | undefined,
-    sceneNumber: string | undefined
-  ) {
-    this.rawText = rawText;
+  get pagesWhole(): number {
+    return Math.floor(this.numLines / this.linesPerPage);
+  }
+
+  get pagesEighth(): number {
+    let remainder = this.numLines / this.linesPerPage - this.pagesWhole;
+    return Math.ceil(remainder * 8);
+  }
+
+  public print(): void {
+    if (this.scene) {
+      let items = this.scene.rowItems;
+      items = items.concat([`${this.numLines}`, `${this.pagesWhole}`, `${this.pagesEighth}`]);
+      let row = items.join('\t');
+      console.log(row);
+    }
+  }
+
+  public handleToken(token: Token) {
+    if (tokenIs(token, TokenType.dual_dialogue_begin)) {
+      this.dualDialogContext = true;
+    }
+    if (tokenIs(token, TokenType.dual_dialogue_end)) {
+      this.dualDialogContext = false;
+    }
+    this.addLinesPrev(token);
+    this.addLines(token);
+    this.prevToken = token;
+  }
+
+  public addLines(token: Token): void {
+    let numLinesWrapped = calcLines(token);
+    // if we're dualDialogContext, use the max
+    if (this.dualDialogContext) {
+      if (numLinesWrapped > this.dualDialogNumLines) {
+        // subtract the old num we used (which may be 0)
+        this.numLines -= this.dualDialogNumLines;
+        this.dualDialogNumLines = numLinesWrapped;
+        // add the new num lines
+        this.numLines += numLinesWrapped;
+      }
+    } else {
+      this.numLines += numLinesWrapped;
+    }
+    // dbg(
+    //   `${token.type} < ${token.text?.slice(0, 10)} > addLines: ${numLinesWrapped} / ${
+    //     this.numLines
+    //   }`
+    // );
+  }
+
+  public addLinesPrev(token: Token) {
+    if (!this.prevToken) {
+      return;
+    }
+    let tokType = tokenType(token.type);
+    switch (tokType) {
+      // scenes are preceded by a double blank line
+      case TokenType.scene_heading:
+        this.numLines += 2;
+      case TokenType.character:
+      case TokenType.action:
+        this.numLines += 1;
+      // TODO: TokenType.page_break
+      default:
+        return;
+    }
+  }
+}
+
+class Report {
+  context: PriorContext;
+  constructor() {
+    this.context = new PriorContext();
+  }
+
+  printAndResetOnNewScene(t: Token) {
+    if (tokenIs(t, TokenType.scene_heading)) {
+      // this won't print the first time we see a scene,
+      // because context.scene is null:
+      this.context.print();
+      this.context = new PriorContext();
+      this.context.scene = Scene.fromToken(t);
+    }
+  }
+
+  public run(tokens: Token[]) {
+    let header = [
+      'ie',
+      'loc',
+      'tod',
+      'sceneNumber',
+      'rawHead',
+      'initialText',
+      'numLines',
+      'numPagesWhole',
+      'numPagesEighth',
+    ];
+    console.log(header.join('\t'));
+    tokens.forEach((t) => {
+      this.context.handleToken(t);
+      this.context.scene?.setInitialText(t);
+      this.printAndResetOnNewScene(t);
+    });
+    this.context.print();
+  }
+}
+
+class Scene {
+  rawHead: string;
+  ie: string;
+  loc?: string;
+  tod?: string;
+  sceneNumber?: string;
+  numLines: number;
+  initialText: string;
+
+  constructor(rawHead: string, ie: string, loc?: string, tod?: string, sceneNumber?: string) {
+    this.rawHead = rawHead;
     this.ie = ie;
     this.loc = loc;
     this.tod = tod;
     this.sceneNumber = sceneNumber;
     this.numLines = 0;
+    this.initialText = '';
   }
 
   static fromToken(token: Token): Scene {
-    let rawText = token.text || '';
+    let rawHead = token.text || '';
     let sceneNumber = token.scene_number;
-    let parts = rawText.split(' - ');
-    let ie = parts[0];
-
-    let loc = undefined;
-    if (parts.length > 1) {
-      loc = parts[1];
-    }
-
-    let tod = undefined;
-    if (parts.length > 2) {
-      tod = parts[2];
-    }
-    return new Scene(rawText, ie, loc, tod, sceneNumber);
+    let sh = parseSceneHeading(rawHead);
+    return new Scene(rawHead, sh.ie, sh.loc, sh.tod, sceneNumber);
   }
 
-  get row(): string {
+  get rowItems(): string[] {
     return [
-      this.ie || '',
-      this.loc || '',
-      this.tod || '',
-      this.sceneNumber || '',
-      this.rawText || '',
-    ]
-      .map((i) => i.replaceAll(/\t/, ''))
-      .join('\t');
+      this.ie || '-',
+      this.loc || '-',
+      this.tod || '-',
+      this.sceneNumber || '-',
+      this.rawHead || '-',
+      this.initialText || '',
+    ].map((i) => i.replaceAll(/\t/g, ''));
+  }
+  get row(): string {
+    return this.rowItems.join('\t');
+  }
+
+  public setInitialText(token: Token) {
+    if (this.initialText == '') {
+      if (token.text != undefined && token.text != '') {
+        this.initialText =
+          token.text
+            .replaceAll(/\t/g, '')
+            .replaceAll(/<[^<]+>/g, '')
+            .slice(0, 30) + '...';
+      }
+    }
   }
 }
 
@@ -168,26 +339,14 @@ const main = () => {
       }),
     (argv) => {
       let fountainFile: string = argv.fountainFile;
-      console.log(`fountainFile: ${fountainFile}`);
       const text = readFileSync(fountainFile, 'utf-8');
-      //   console.log(text);
       let fountain = new Fountain();
       let f = fountain.parse(text, true);
-      f.tokens?.forEach((t) => {
-        console.log(tokenType(t.type));
-        if (tokenIs(t, TokenType.scene_heading)) {
-          console.log('-------------------');
-          let scene = Scene.fromToken(t);
-          console.log(scene.row);
-        }
-      });
-      console.log(TokenType['scene_heading'] === TokenType.scene_heading);
+
+      let report = new Report();
+      report.run(f.tokens || []);
     }
   ).argv;
-
-  const d = `${new Date()}`;
-  console.log(d);
-  console.log(d);
 };
 
 export const foo = () => {
--- src/test/jest/index.test.ts
+++ src/test/jest/index.test.ts
@@ -1,5 +1,5 @@
 import 'jest';
-import { foo, calcLines } from '../../index';
+import { foo, calcLines, cleanCharacter, parseSceneHeading } from '../../index';
 
 test('foo', () => {
   expect(foo()).toBe(1);
@@ -20,4 +20,17 @@ test('calcLines', () => {
   expect(calcLines({ type: 'action', text: forty })).toBe(1);
   expect(calcLines({ type: 'action', text: eighty })).toBe(2);
   expect(calcLines({ type: 'action', text: oneSixty })).toBe(3);
+});
+
+test('parseSceneHeading', () => {
+  let t = 'INT. ELLIE’S KITCHEN - NIGHT [LATER]';
+  expect(parseSceneHeading(t).rawHead).toBe(t);
+  expect(parseSceneHeading(t).ie).toBe('INT.');
+});
+
+test('cleanCharacter', () => {
+  expect(cleanCharacter("PAUL (V.O.) (O.S) (CONT'D)")).toBe('PAUL');
+  expect(cleanCharacter("PAUL (V.O.) (O.S) (CONT'D) ^")).toBe('PAUL');
+  expect(cleanCharacter('DR. WALTER VON BRAUN')).toBe('DR. WALTER VON BRAUN');
+  expect(cleanCharacter('@PAM')).toBe('PAM');
 });
